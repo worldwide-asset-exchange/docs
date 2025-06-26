1a73a5e36b6e98d600742c60e0c14c299fd880dd
includenull <15929809+includenull@users.noreply.github.com>
2025-06-26 06:03:01 +0100

    Update documentation links to point to the correct repository

--- docs/.vitepress/locales/cn/lang.ts
+++ docs/.vitepress/locales/cn/lang.ts
@@ -8,7 +8,7 @@ export default {
   title: 'WAX Docs',
   description: '开发者门户在WAX区块链',
   editLink: {
-    pattern: 'https://github.com/wax-office-of-inspector-general/wax-developer/edit/main/docs/:path',
+    pattern: 'https://github.com/worldwide-asset-exchange/docs/edit/main/docs/:path',
     text: 'Improve this page on GitHub'
   },
   themeConfig: {
